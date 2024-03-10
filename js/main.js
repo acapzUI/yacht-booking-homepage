025b5a39c865fa76bce8afa9ce7ea5d0cdcf303e
Minjea Kim <jbm98@naver.com>
2024-03-10 10:52:36 +0900

    3/10

--- js/main.js
+++ js/main.js
@@ -1,28 +1,6 @@
 'use strict'
 
 /**
- * 검색창 제어
- */
-// 검색창 요소(.search) 찾기.
-const searchEl = document.querySelector('.search')
-const searchInputEl = searchEl.querySelector('input')
-// 검색창 요소를 클릭하면 실행.
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus()
-})
-// 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused')
-  searchInputEl.setAttribute('placeholder', '통합검색')
-})
-// 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused')
-  searchInputEl.setAttribute('placeholder', '')
-})
-
-
-/**
  * 페이지 스크롤에 따른 요소 제어
  */
 // 페이지 스크롤에 영향을 받는 요소들을 검색!
@@ -88,36 +66,22 @@ new Swiper('.notice-line .swiper-container', {
   autoplay: true, // 자동 재생 여부
   loop: true // 반복 재생 여부
 })
-new Swiper('.promotion .swiper-container', {
-  // direction: 'horizontal', // 수평 슬라이드
-  autoplay: { // 자동 재생 여부
-    delay: 5000 // 5초마다 슬라이드 바뀜
+new Swiper(".yacht-swiper", {
+  autoplay: {
+    delay: 5000
   },
-  loop: true, // 반복 재생 여부
-  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
-  spaceBetween: 10, // 슬라이드 사이 여백
-  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
-  pagination: { // 페이지 번호 사용 여부
-    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
-    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  loop: true,
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
-    prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
-    nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
-  }
-})
-new Swiper('.awards .swiper-container', {
-  // direction: 'horizontal', // 수평 슬라이드
-  autoplay: true, // 자동 재생 여부
-  loop: true, // 반복 재생 여부
-  spaceBetween: 30, // 슬라이드 사이 여백
-  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
-  // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
-  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
-    prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
-    nextEl: '.awards .swiper-next' // 다음 버튼 선택자
-  }
-})
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 
 /**
@@ -181,7 +145,7 @@ spyEls.forEach(function (spyEl) {
   new ScrollMagic
     .Scene({ // 감시할 장면(Scene)을 추가
       triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
-      triggerHook: 0.8 // 화면의 80% 지점에서 보여짐 여부 감시
+      triggerHook: 0.8, // 화면의 80% 지점에서 보여짐 여부 감시
     })
     .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
