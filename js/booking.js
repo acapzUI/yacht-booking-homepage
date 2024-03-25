1a0564c6c3cd01f597f798ef5b4216bf5e9c9a9e
Minjea Kim <jbm98@naver.com>
2024-03-25 15:02:59 +0900

    3.25

--- js/booking.js
+++ js/booking.js
@@ -1,17 +1,34 @@
-(function () {
-    'use strict';
-    var init = function () {                
-        var slider = new rSlider({
-            target: '#slider',
-            values: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-            range: true,
-            set: [0, 1],
-            scale: true,
-            tooltip: true,
-            onChange: function (vals) {
-                console.log(vals);
-            }
-        });
-    };
-    window.onload = init;
-})();
+// Javascript Date System 안쓰고 Java API를 추후에 개발해서 사용
+// 
+
+
+let today = new Date();
+let now_year = today.getFullYear();
+let now_month = today.getMonth();
+let now_date = today.getDate();
+let now_day = today.getDay()
+
+
+if (now_month == 12) {
+    let next_month = new Date(now_year+1, 1);
+} else {
+    let next_month = new Date(now_year, now_month+1);
+}
+
+if (now_month == 1) {
+    let prev_month = new Date(now_year-1, 12);
+} else {
+    let prev_month = new Date(now_year, now_month-1);
+}
+
+let size_month = getDateDiff("".contcat(next_month).contcat("-").contcat)
+
+
+const getDateDiff = (d1, d2) => {
+    const date1 = new Date(d1);
+    const date2 = new Date(d2);
+    
+    const diffDate = date1.getTime() - date2.getTime();
+    
+    return Math.abs(diffDate / (1000 * 60 * 60 * 24)); // 밀리세컨 * 초 * 분 * 시 = 일
+  }
